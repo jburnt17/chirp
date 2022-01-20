d5bdd7d366875be0559d4760a3979482e513379a
Jared B <jburnt17@gmail.com>
2022-01-20 14:24:25 -0500

    finished user options styling

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -1,17 +1,32 @@
 import { UserIcon } from "@heroicons/react/outline";
 import { HomeIcon } from "@heroicons/react/solid";
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
+import { logout } from "../../store/session";
 import LogoutButton from "../auth/LogoutButton";
 import "./NavBar.css";
 
 const NavBar = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.session.user);
+  const [showUserOptions, setShowUserOptions] = useState(false);
+
+  const onLogout = async (e) => {
+    await dispatch(logout());
+  };
+
   return (
     <nav className="nav-bar-container">
       <ul className="nav-bar-list">
         <li>
           <HomeIcon className="nav-icon" />
-          <NavLink className="home-text" to="/" exact={true} activeClassName="active">
+          <NavLink
+            className="home-text"
+            to="/"
+            exact={true}
+            activeClassName="active"
+          >
             Home
           </NavLink>
         </li>
@@ -34,11 +49,37 @@ const NavBar = () => {
           </NavLink>
         </li>
         <button className="nav-lobby-button">Create Lobby</button>
-        {/* <LogoutButton/> */}
-        <div className="user-profile-container">
-          
-        </div>
       </ul>
+      <div
+        className="user-profile-container"
+        onClick={() => setShowUserOptions(!showUserOptions)}
+      >
+        <img
+          className="nav-avatar"
+          src="https://raw.githubusercontent.com/jburnt17/chirp/80e5df043874ef4ce9a3dd3398a99d070d63fdf5/react-app/public/user-avatar.svg"
+        />
+        <div>
+          <p className="nav-username">{user.username}</p>
+          <p className="nav-email">{user.email}</p>
+        </div>
+
+        {showUserOptions && (
+          <div className="nav-user-options">
+            <div className="user-profile-options">
+              <img
+                className="nav-options-avatar"
+                src="https://raw.githubusercontent.com/jburnt17/chirp/80e5df043874ef4ce9a3dd3398a99d070d63fdf5/react-app/public/user-avatar.svg"
+              />
+              <div>
+                <p className="nav-username">{user.username}</p>
+                <p className="nav-email">{user.email}</p>
+              </div>
+            </div>
+            <button className="logout-button" onClick={onLogout}>Logout</button>
+            <div className="triangle"></div>
+          </div>
+        )}
+      </div>
     </nav>
   );
 };
