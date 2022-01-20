ba605f12440a3d70d9c21f86879a2d1e41d2c617
Jared B <jburnt17@gmail.com>
2022-01-20 02:17:29 -0500

    added basic sign up styles

--- react-app/src/components/auth/SignUpForm/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm/SignUpForm.js
@@ -1,23 +1,24 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux'
-import { Redirect } from 'react-router-dom';
-import { signUp } from '../../../store/session';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Redirect, useHistory } from "react-router-dom";
+import { signUp } from "../../../store/session";
 
 const SignUpForm = () => {
   const [errors, setErrors] = useState([]);
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
-  const user = useSelector(state => state.session.user);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+  const user = useSelector((state) => state.session.user);
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const onSignUp = async (e) => {
     e.preventDefault();
     if (password === repeatPassword) {
       const data = await dispatch(signUp(username, email, password));
       if (data) {
-        setErrors(data)
+        setErrors(data);
       }
     }
   };
@@ -39,55 +40,72 @@ const SignUpForm = () => {
   };
 
   if (user) {
-    return <Redirect to='/' />;
+    return <Redirect to="/" />;
   }
 
   return (
-    <form onSubmit={onSignUp}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
+    <div className="login-body">
+      <video className="left-login" muted autoPlay loop>
+        <source className="login-image" src="/hockey-video.mp4" />
+      </video>
+      <div className="right-login">
+        <form className="login-form" onSubmit={onSignUp}>
+          <h3 className="login-title">Sign up for Chirp</h3>
+          <div>
+            {errors.map((error, ind) => (
+              <div key={ind}>{error}</div>
+            ))}
+          </div>
+          <div className="login-input-wrapper">
+            <input
+              className="login-input"
+              type="text"
+              name="username"
+              onChange={updateUsername}
+              value={username}
+            ></input>
+            <label className="login-label">Username</label>
+          </div>
+          <div className="login-input-wrapper">
+            <input
+              className="login-input"
+              type="text"
+              name="email"
+              onChange={updateEmail}
+              value={email}
+            ></input>
+            <label className="login-label">Email</label>
+          </div>
+          <div className="login-input-wrapper">
+            <input
+              className="login-input"
+              type="password"
+              name="password"
+              onChange={updatePassword}
+              value={password}
+            ></input>
+            <label className="login-label">Password</label>
+          </div>
+          <div className="login-input-wrapper">
+            <input
+              className="login-input"
+              type="password"
+              name="repeat_password"
+              onChange={updateRepeatPassword}
+              value={repeatPassword}
+              required={true}
+            ></input>
+            <label className="login-label">Repeat Password</label>
+          </div>
+          <button className="login-button" type="submit">Sign Up</button>
+          <div className="login-signup-link-con">
+            <p>
+              Already have an account? <span onClick={() => history.push('/login')} className="login-signup-link">Login</span>
+            </p>
+          </div>
+        </form>
       </div>
-      <div>
-        <label>User Name</label>
-        <input
-          type='text'
-          name='username'
-          onChange={updateUsername}
-          value={username}
-        ></input>
-      </div>
-      <div>
-        <label>Email</label>
-        <input
-          type='text'
-          name='email'
-          onChange={updateEmail}
-          value={email}
-        ></input>
-      </div>
-      <div>
-        <label>Password</label>
-        <input
-          type='password'
-          name='password'
-          onChange={updatePassword}
-          value={password}
-        ></input>
-      </div>
-      <div>
-        <label>Repeat Password</label>
-        <input
-          type='password'
-          name='repeat_password'
-          onChange={updateRepeatPassword}
-          value={repeatPassword}
-          required={true}
-        ></input>
-      </div>
-      <button type='submit'>Sign Up</button>
-    </form>
+    </div>
   );
 };
 
