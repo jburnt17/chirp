236348f1abc6c0bac79d3f37d78979ca6672d242
Jared B <jburnt17@gmail.com>
2022-01-24 14:10:11 -0500

    added game lobby page styles

--- react-app/src/components/GameLobbyPage/GameLobbyPage.js
+++ react-app/src/components/GameLobbyPage/GameLobbyPage.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { PaperAirplaneIcon } from "@heroicons/react/solid";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getGames } from "../../store/games";
@@ -12,6 +13,7 @@ import { getTodaysGames } from "../../store/todaysGames";
 import NavBar from "../NavBar/NavBar";
 import TodaysGames from "../TodaysGames/TodaysGames";
 import "./GameLobbyPage.css";
+import { XCircleIcon } from "@heroicons/react/outline";
 
 function GameLobbyPage() {
   const dispatch = useDispatch();
@@ -92,84 +94,124 @@ function GameLobbyPage() {
         <NavBar />
       </div>
       <div className="game-lobby-middle">
-        <div>
-          <div>
-            <div>{home && home.team.name}</div>
-            <div>
-              {home &&
-                `${home.leagueRecord.wins} - ${home.leagueRecord.losses} - ${home.leagueRecord.ot}`}
-            </div>
-          </div>
-          <div>{home && home.score}</div>
-        </div>
-
-        <div>
-          <div>
-            <div>{away && away.team.name}</div>
-            <div>
-              {away &&
-                `${away.leagueRecord.wins} - ${away.leagueRecord.losses} - ${away.leagueRecord.ot}`}
-            </div>
-          </div>
-          <div>{away && away.score}</div>
-        </div>
-
-        {!editState ? (
-          <form
-            className="chirp-bottom-input"
-            onSubmit={(e) => handleChirpSubmit(e)}
-          >
-            <textarea
-              onKeyPress={(e) => handleEnter(e)}
-              placeholder="chirp"
-              onChange={(e) => setContent(e.target.value)}
-              type="submit"
-              value={content}
-              id="chirp-text-area"
-            />
-            <button>Chirp</button>
-          </form>
-        ) : (
-          <form
-            className="chirp-bottom-input"
-            onSubmit={(e) => handleEditChirp(e)}
-          >
-            <textarea
-              onKeyPress={(e) => handleEnter(e)}
-              id="chirp-text-area"
-              onChange={(e) => setContent(e.target.value)}
-              type="submit"
-              value={content}
-            />
-            <button>Edit Chirp</button>
-            <button
-              onClick={() => {
-                setEditState(false);
-                setContent("");
-              }}
-            >
-              Cancel
-            </button>
-          </form>
-        )}
-        {gameLobbyChirps.map((chirp) => (
-          <div>
-            <div>{chirp.content}</div>
-            {chirp.user_id === sessionUser.id && (
+        <div className="game-lobby-teams">
+          <div className="game-lobby-team-info">
+            <div className="game-lobby-team-stats">
+              {home && home.team.name !== "Seattle Kraken" ? (
+                <img
+                  width={128}
+                  src={`https://www-league.nhlstatic.com/images/logos/teams-20202021-light/${home?.team.id}.svg
+`}
+                />
+              ) : (
+                <img
+                  width={28}
+                  src={`https://www-league.nhlstatic.com/images/logos/teams-20202021-light/${home?.team.id}.svg
+`}
+                />
+              )}
+              <div>{home && home.team.name}</div>
               <div>
-                <button onClick={() => dispatch(deleteChirp(gameId, chirp.id))}>
-                  Delete
-                </button>
+                {home &&
+                  `${home.leagueRecord.wins} - ${home.leagueRecord.losses} - ${home.leagueRecord.ot}`}
+              </div>
+            </div>
+            <div className="game-lobby-score">{home && home.score}</div>
+          </div>
+
+          <div className="game-lobby-team-info">
+            <div className="game-lobby-score">{away && away.score}</div>
+            <div className="game-lobby-team-stats">
+              {away && away.team.name !== "Seattle Kraken" ? (
+                <img
+                  width={128}
+                  src={`https://www-league.nhlstatic.com/images/logos/teams-20202021-light/${away?.team.id}.svg
+`}
+                />
+              ) : (
+                <img
+                  width={28}
+                  src={`https://www-league.nhlstatic.com/images/logos/teams-20202021-light/${away?.team.id}.svg
+`}
+                />
+              )}
+              <div>{away && away.team.name}</div>
+              <div>
+                {away &&
+                  `${away.leagueRecord.wins} - ${away.leagueRecord.losses} - ${away.leagueRecord.ot}`}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="chirps-container">
+          {gameLobbyChirps.map((chirp) => (
+            <div>
+              <div>{chirp.content}</div>
+              {chirp.user_id === sessionUser.id && (
+                <div>
+                  <button
+                    onClick={() => dispatch(deleteChirp(gameId, chirp.id))}
+                  >
+                    Delete
+                  </button>
+                  <button
+                    className="edit-chirp-button"
+                    onClick={() => handleEditState(chirp.content, chirp.id)}
+                  >
+                    Edit
+                  </button>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+        <div className="game-lobby-input-container">
+          {!editState ? (
+            <form
+              className="chirp-bottom-input"
+              onSubmit={(e) => handleChirpSubmit(e)}
+            >
+              <textarea
+                onKeyPress={(e) => handleEnter(e)}
+                placeholder="Start chirping..."
+                onChange={(e) => setContent(e.target.value)}
+                type="submit"
+                value={content}
+                id="chirp-text-area"
+              />
+              <button className="reg-chirp-button chirp-button">
+                <PaperAirplaneIcon width={20} />
+              </button>
+            </form>
+          ) : (
+            <form
+              className="chirp-bottom-input"
+              onSubmit={(e) => handleEditChirp(e)}
+            >
+              <textarea
+                onKeyPress={(e) => handleEnter(e)}
+                id="chirp-text-area"
+                onChange={(e) => setContent(e.target.value)}
+                type="submit"
+                value={content}
+              />
+              <div className="chirp-edit-buttons">
+                {/* <button className="chirp-button">
+                  <PaperAirplaneIcon width={20} />
+                </button> */}
                 <button
-                  className="edit-chirp-button"
-                  onClick={() => handleEditState(chirp.content, chirp.id)}
+                  className="chirp-button"
+                  onClick={() => {
+                    setEditState(false);
+                    setContent("");
+                  }}
                 >
-                  Edit
+                  <XCircleIcon className="cancel-edit-button" width={22} />
                 </button>
               </div>
-            )}
-          </div>
-        ))}
+            </form>
+          )}
+        </div>
       </div>
       <div className="game-lobby-right">
         <TodaysGames gamesToday={gamesToday} />
