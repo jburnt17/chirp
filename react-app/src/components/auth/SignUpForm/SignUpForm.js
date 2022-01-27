28ba953062746a49c441c51552f2d2f8cdf46e38
Jared B <jburnt17@gmail.com>
2022-01-27 13:05:15 -0500

    added sign in error handling

--- react-app/src/components/auth/SignUpForm/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm/SignUpForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Redirect, useHistory } from "react-router-dom";
 import { signUp } from "../../../store/session";
@@ -13,12 +13,18 @@ const SignUpForm = () => {
   const dispatch = useDispatch();
   const history = useHistory();
 
+  useEffect(() => {
+    const errors = [];
+    if (password !== repeatPassword) errors.push("Passwords do not match.");
+    setErrors([...errors]);
+  }, [repeatPassword]);
+
   const onSignUp = async (e) => {
     e.preventDefault();
-    if (password === repeatPassword) {
+    if (!errors.length) {
       const data = await dispatch(signUp(username, email, password));
       if (data) {
-        setErrors(data);
+        setErrors([...errors, data]);
       }
     }
   };
@@ -39,71 +45,141 @@ const SignUpForm = () => {
     setRepeatPassword(e.target.value);
   };
 
+  useEffect(() => {
+    console.log(errors);
+    console.log(errors[0]?.includes("username : Username is already in use."));
+    console.log(
+      errors[0]?.includes("email : Email address is already in use.")
+    );
+  }, [errors]);
+
   if (user) {
     return <Redirect to="/" />;
   }
 
   return (
     <div className="login-body">
+      {/* <div>
+        {errors.length
+          ? errors.map((error, ind) => <div key={ind}>{error}</div>)
+          : null}
+      </div> */}
       <video className="left-login" muted autoPlay loop>
-        <source className="login-image" src="https://jmb-s3-bucket.s3.amazonaws.com/hockey-video.mp4" />
+        <source
+          className="login-image"
+          src="https://jmb-s3-bucket.s3.amazonaws.com/hockey-video.mp4"
+        />
       </video>
       <div className="right-login">
         <form className="login-form" onSubmit={onSignUp}>
           <h3 className="login-title">Sign up for Chirp</h3>
-          <div>
-            {errors.map((error, ind) => (
-              <div key={ind}>{error}</div>
-            ))}
-          </div>
           <div className="login-input-wrapper">
             <input
-              className="login-input"
+              className={
+                !errors[0]?.includes("username : Username is already in use.")
+                  ? `login-input`
+                  : `login-input-error`
+              }
               type="text"
               name="username"
               onChange={updateUsername}
               value={username}
               required={true}
             ></input>
-            <label className="login-label">Username</label>
+            <label
+              className={
+                !errors[0]?.includes("username : Username is already in use.")
+                  ? `login-label`
+                  : `login-label-error`
+              }
+            >
+              Username
+            </label>
+            {errors[0]?.includes("username : Username is already in use.") && <div className="error-label">Username is already in use.</div>}
           </div>
           <div className="login-input-wrapper">
             <input
-              className="login-input"
+              className={
+                !errors[0]?.includes("email : Email address is already in use.")
+                  ? `login-input`
+                  : `login-input-error`
+              }
               type="text"
               name="email"
               onChange={updateEmail}
               value={email}
               required={true}
             ></input>
-            <label className="login-label">Email</label>
+            <label
+              className={
+                !errors[0]?.includes("email : Email address is already in use.")
+                  ? `login-label`
+                  : `login-label-error`
+              }
+            >
+              Email
+            </label>
+            {errors[0]?.includes("email : Email address is already in use.") && <div className="error-label">Email is already in use.</div>}
           </div>
           <div className="login-input-wrapper">
             <input
-              className="login-input"
+              className={
+                !errors.includes("Passwords do not match.")
+                  ? `login-input`
+                  : `login-input-error`
+              }
               type="password"
               name="password"
               onChange={updatePassword}
               value={password}
               required={true}
             ></input>
-            <label className="login-label">Password</label>
+            <label
+              className={
+                !errors.includes("Passwords do not match.")
+                  ? `login-label`
+                  : `login-label-error`
+              }
+            >
+              Password
+            </label>
           </div>
           <div className="login-input-wrapper">
             <input
-              className="login-input"
+              className={
+                !errors.includes("Passwords do not match.")
+                  ? `login-input`
+                  : `login-input-error`
+              }
               type="password"
               name="repeat_password"
               onChange={updateRepeatPassword}
               value={repeatPassword}
               required={true}
             ></input>
-            <label className="login-label">Repeat Password</label>
+            <label
+              className={
+                !errors.includes("Passwords do not match.")
+                  ? `login-label`
+                  : `login-label-error`
+              }
+            >
+              Repeat Password
+            </label>
+            {errors[0]?.includes("Passwords do not match.") && <div className="error-label">Passwords do not match.</div>}
           </div>
-          <button className="login-button" type="submit">Sign Up</button>
+          <button className="login-button" type="submit">
+            Sign Up
+          </button>
           <div className="login-signup-link-con">
             <p>
-              Already have an account? <span onClick={() => history.push('/login')} className="login-signup-link">Login</span>
+              Already have an account?{" "}
+              <span
+                onClick={() => history.push("/login")}
+                className="login-signup-link"
+              >
+                Login
+              </span>
             </p>
           </div>
         </form>
